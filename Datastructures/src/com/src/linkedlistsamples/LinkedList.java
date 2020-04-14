3e48ff60789014e92c54ae416540543811784e88
Kamalakannan Gnanasekar <k.gnanasekar@wolterskluwer.com>
2020-04-14 20:01:49 +0530

    Move datastructures folder

---
+++ Datastructures/src/com/src/linkedlistsamples/LinkedList.java
@@ -0,0 +1,115 @@
+package com.src.linkedlistsamples;
+import java.io.*;
+
+import javax.swing.text.Position;
+
+
+// Java program to implement 
+// a Singly Linked List 
+public class LinkedList<T>  { 
+
+	static class Node<T>{
+		private Node<T> next;
+		private Node<T> prev;
+		private T data;
+		public Node<T> getNext() {
+			return next;
+		}
+		public void setNext(Node<T> next) {
+			this.next = next;
+		}
+		public Node<T> getPrev() {
+			return prev;
+		}
+		public void setPrev(Node<T> prev) {
+			this.prev = prev;
+		}
+		public T getData() {
+			return data;
+		}
+		public void setData(T data) {
+			this.data = data;
+		}
+	}
+	
+	private Node<T> first;
+	private Node<T> last;
+	public Node<T> getFirst() {
+		return first;
+	}
+	public void setFirst(Node<T> first) {
+		this.first = first;
+	}
+	public Node<T> getLast() {
+		return last;
+	}
+	public void setLast(Node<T> last) {
+		this.last = last;
+	}
+	public LinkedList() {
+        first = last = null;
+    }
+	public boolean isEmpty() {
+		return first == null;
+	}
+	
+	
+	public void add(T data) {
+		Node<T> temp = new Node<T>();
+		temp.setPrev(null);
+		temp.setData(data);
+		if(first == null) {
+			temp.setPrev(null);
+			first=last=temp;
+		}else {
+			temp.setPrev(last);
+			last.setNext(temp);
+			last = temp;
+		}
+	} 
+	public void remove(int position) {
+		Node<T> temp = new Node<>();
+		temp = first;
+		if(first==null) {
+			return;
+		}
+		else if(position==0) {
+			first = temp.next;
+			return;
+		}
+		for(int i=0; i<position-1; i++)
+			temp = temp.next;//next of temp node is the node given to remove
+		
+		if(temp==null && temp.next==null)
+			return;
+		
+		Node<T> nextNode = new Node<>();
+		nextNode = temp.next;
+		temp.next = nextNode.next;
+		if(temp.next == null)
+			last = temp;
+		
+	}
+	public int getCount() 
+    { 
+       /* Node<T> temp = first; 
+        int count = 0; 
+        while (temp != null) 
+        { 
+            count++; 
+            temp = temp.next; 
+        } 
+        return count; */
+		Node<T> temp = first;
+		int count = 0;
+		if(temp!=null) {
+			count = 1;
+			while(temp.next!=null) {
+				temp = temp.next;
+				count++;
+			}
+			return count;
+		}
+		return count;
+    } 
+} 
---
+++ Datastructures/src/com/src/linkedlistsamples/Main.java
@@ -0,0 +1,48 @@
+package com.src.linkedlistsamples;
+
+import com.src.linkedlistsamples.LinkedList.Node;
+
+public class Main {
+
+	public Node<Integer> findMiddleNode(Node<Integer> first)
+	{
+		Node<Integer> slowPointer, fastPointer; 
+		slowPointer = fastPointer = first; 
+ 
+		while(fastPointer !=null) { 
+			fastPointer = fastPointer.getNext(); 
+			if(fastPointer != null && fastPointer.getNext() != null) { 
+				slowPointer = slowPointer.getNext(); 
+				fastPointer = fastPointer.getNext(); 
+			} 
+		} 
+ 
+		return slowPointer; 
+ 
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int node1=1;
+		int node2=2;
+		int node3=3;
+		int node4=4;
+		LinkedList<Integer> obj = new LinkedList<Integer>();
+		obj.add(node1);
+		obj.add(node2);
+		obj.add(node3);
+		obj.add(node4);
+		obj.add(5);
+		obj.add(6);
+		obj.add(7);
+		obj.add(8);
+		obj.add(9);
+		obj.add(10);
+		System.out.println(obj.getLast().getData());
+		System.out.println("count===>"+obj.getCount());
+		//obj.remove(3);
+		System.out.println(obj.getLast().getData());
+		System.out.println("count===>"+obj.getCount());
+		Node<Integer> middle = new Main().findMiddleNode(obj.getFirst());
+		System.out.println(middle.getData());
+	}
+}
